0d380b8e1799253c71f85bd22a3232a45467d447
Mohd Hussain <samsteve124@gmail.com>
2024-06-15 19:05:58 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+
+import ComingSoonVersion from "@/components/ComingSoonVersion/ComingSoonVersion";
+
+export default function Home() {  
+  return (    
+      <>       
+        {/* Standard Version */}
+        <ComingSoonVersion />
+      </>
+  );
+}
---
+++ src/components/ComingSoonVersion/ComingSoonVersion.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import CountDown from '../CountDown/Countdown'
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+function ComingSoonVersion1() {
+    const specificEndDate = new Date('2024-10-31T23:59:59');
+
+  return (
+    <>
+        <section className="flex min-h-screen flex-col w-full items-center justify-between p-5 lg:p-12">
+            <Header />
+            <CountDown endDate={specificEndDate} />
+            
+            <Footer />
+        </section>
+    </>
+  )
+}
+
+export default ComingSoonVersion1
