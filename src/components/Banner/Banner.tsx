41e797b4e3dee309422291cbc38a28df7bcf03f5
Mohd Hussain <samsteve124@gmail.com>
2024-06-16 20:45:41 +0530

    &apos; typo fixed

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -9,9 +9,9 @@ const Banner = () => {
         <h2 className='text-4xl font-bold'>
           Knowledge + Practise = <span className='text-teal-700'>Success</span>
         </h2>
-        <p className=' mt-5 text-lg'>
-          Don't just memorize, master your exams. Mockyard puts your knowledge to the test with a
-          massive library, personalized insights, and expert-crafted materials. Transform learning
+        <p className='mt-5 text-lg'>
+          Don&apos;t just memorize, master your exams. Mockyard puts your knowledge to the test with
+          a massive library, personalized insights, and expert-crafted materials. Transform learning
           into exam victory. Start practicing today!
         </p>
         <button className='bg-slate-950 shadow-sm py-4 px-3 mt-10 text-white rounded-lg w-[150px] '>
