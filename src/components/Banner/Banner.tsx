6c40f4aae2ae151beeaf6f2b4be679eca1217f5d
Mohd Hussain <samsteve124@gmail.com>
2024-06-20 19:07:04 +0530

    apox added

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -10,8 +10,8 @@ const Banner = () => {
           <Image src='/study.svg' width={400} height={300} alt='study' />
           <div className='flex  flex-col  h-[60%]'>
             <p className='mt-6 text-gray-500'>
-              Don't just memorize, master your exams. Mockyard puts your knowledge to the test with
-              a massive library, personalized insights, and expert-crafted materials. Transform
+              Don&apost just memorize, master your exams. Mockyard puts your knowledge to the test
+              with a massive library, personalized insights, and expert-crafted materials. Transform
               learning into exam victory. Start practicing today!
             </p>
             <button className='text-white mt-4 py-4 px-3 rounded-lg bg-primary w-[200px]'>
