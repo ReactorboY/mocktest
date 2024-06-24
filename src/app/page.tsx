5457be89046a7a56d4298a0bbec9eb51332c77d4
Mohd Hussain <samsteve124@gmail.com>
2024-06-24 10:54:29 +0530

    session user added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   const specificEndDate = new Date('2024-10-31T23:59:59')
   const session = await auth()
 
-  if (session) {
+  if (session?.user) {
     redirect('/dashboard')
   }
 
